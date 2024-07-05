71f27ea2a75e4c533b4ac7c8a6cae6b7ce6d996b
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2024-07-05 15:13:23 +0100

    Time: 104 ms (38.68%), Space: 45.7 MB (68%) - LeetHub

--- 0027-remove-element/0027-remove-element.cs
+++ 0027-remove-element/0027-remove-element.cs
@@ -1,19 +1,15 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        int[] res = new int[nums.Length];
-        int j = 0;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (nums[i] != val)
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                res[j] = nums[i];
-                j++;
+                if (nums[i] != val)
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
             }
-        }
-        for (int i = 0; i < j; i++)
-        {
-            nums[i] = res[i];
-        }
-        return j;
+
+            return j;
     }
 }
