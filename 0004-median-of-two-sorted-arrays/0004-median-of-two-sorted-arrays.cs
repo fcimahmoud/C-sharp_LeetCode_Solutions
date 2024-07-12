d9bbb472f0625f2235fbcb802bb441a17325372b
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2024-07-12 09:46:47 +0100

    Time: 100 ms (37.16%), Space: 55.1 MB (27.69%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -1,17 +1,37 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-            int m = nums1.Length, n = nums2.Length;
-            int[] mergedArr = new int[m + n];
+        int m = nums1.Length, n = nums2.Length;
+        int[] mergedArr = new int[m + n];
 
-            for (int i = 0; i < m; i++)
-                mergedArr[i] = nums1[i];
-            for (int i = m; i < m + n; i++)
-                mergedArr[i] = nums2[i - m];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n)
+        {
+            if (nums1[i] < nums2[j])
+            { 
+                mergedArr[k] = nums1[i]; 
+                i++; 
+            }
+            else
+            {
+                mergedArr[k] = nums2 [j];
+                j++;
+            }
+            k++;
+        }
+        while(i < m)
+        {
+            mergedArr[k] = nums1[i];
+            k++; i++;
+        }
+        while (j < n)
+        {
+            mergedArr[k] = nums2[j];
+            k++; j++;
+        }
 
-            Array.Sort(mergedArr);
-            if ((m + n) % 2 == 0)
-                return (double)(mergedArr[(m + n) / 2] + mergedArr[((m + n) / 2) - 1]) / 2;
-            else 
-                return mergedArr[(m + n) / 2];
+        if ((m + n) % 2 == 0)
+            return (double)(mergedArr[(m + n) / 2] + mergedArr[((m + n) / 2) - 1]) / 2;
+        else
+            return mergedArr[(m + n) / 2];
     }
 }
