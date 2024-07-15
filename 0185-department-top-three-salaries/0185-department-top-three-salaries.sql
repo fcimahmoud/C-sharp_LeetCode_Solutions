a684ddcf5f7cd4e3de90e0a2a3cb11d428a64fa7
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2024-07-15 13:15:39 +0100

    Time: 414 ms (98.77%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 Select d.name as Department, e.name as Employee, e.salary as Salary
 From
     ( Select *, DENSE_RANK() Over (Partition by departmentId Order by salary desc) as SalaryRank
